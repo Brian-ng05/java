c54a77b8409268fa230aa6ef8a74f27cfcf087fd
Brian <nguyenbao12122005@gmail.com>
2025-06-17 15:35:25 +0700

    update

--- src/TaskManagement/V02/TaskManagement.java
+++ src/TaskManagement/V02/TaskManagement.java
@@ -140,6 +140,8 @@ public class TaskManagement {
                     break;
                 default:
                     System.out.println("Invalid option. Please select a valid option (1-5).\n");
+                    System.out.println("Invalid option. Please select a valid option (1-5).\n");
+                    System.out.println("Invalid option. Please select a valid option (1-5).\n");
                     break;
             }
         } while (running);
